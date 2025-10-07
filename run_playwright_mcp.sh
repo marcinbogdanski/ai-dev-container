9ce535d211983b560a905bb1835b2e7291c6569d
Marcin Bogdanski <marcin.bogdanski@gmail.com>
2025-10-07 15:12:14 +0200

    Add run_playwright_mcp.sh to launch companion container

---
+++ run_playwright_mcp.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Variables
+PLAYWRIGHT_OUTPUT_DIR="$HOME/Tmp/playwright-mcp-output"
+mkdir -p "$PLAYWRIGHT_OUTPUT_DIR"
+
+docker run -d \
+  --name playwright-mcp \
+  -p 0.0.0.0:8931:8931 \
+  -v $PLAYWRIGHT_OUTPUT_DIR:/tmp/playwright-mcp-output \
+  playwright-mcp --port 8931 --host 0.0.0.0
